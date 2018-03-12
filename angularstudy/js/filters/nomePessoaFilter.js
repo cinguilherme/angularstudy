55dd6f4e21a7a849f20d902ceafba2aca0124991
Cintra, Guilherme <cinguilherme@gmail.com>
2018-03-12 19:05:26 -0300

    filtro nome pessoa funcionando.

--- angularstudy/js/filters/nomePessoaFilter.js
+++ angularstudy/js/filters/nomePessoaFilter.js
@@ -1,8 +1,17 @@
 angular.module("listaTelefonica").filter("nomePessoa", function(){
 
   return function(input) {
-    console.log(input);
-    return input;
+
+    var nomes = input.split(" ");
+    var nomeFiltrado = "";
+
+    for(var i = 0; i < nomes.length; i++){
+      nomeFiltrado += nomes[i].length > 2? nomes[i][0].toUpperCase()+nomes[i].substring(1) + " ": nomes[i]+" ";
+    }
+
+    console.log(nomeFiltrado.trim());
+
+    return nomeFiltrado;
   };
 
 });
